5a286d7d5d2b30ab7abf4ecced43365e7991c9c6
SC-McD <stewart.charles.mcdowall@protonmail.ch>
2023-07-28 13:00:35 +0200

    update docs

--- docs/make_docs.sh
+++ docs/make_docs.sh
@@ -4,7 +4,7 @@ ghpush update-docs
 git checkout main
 cp -R docs/_build/html ~/tmp/futuram-ghpages
 git checkout gh-pages
-cp -R ~/tmp/futuram-ghpages/* .
+cp -R ~/tmp/futuram-ghpages/html/* .
 git add .
 git commit -m "update docs"
 git push
