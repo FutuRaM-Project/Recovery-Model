98247523c571a25b746596927457b6ded43dd390
SC-McD <stewart.charles.mcdowall@protonmail.ch>
2023-07-28 12:39:01 +0200

    initial

--- src/build/lib/futuram/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import classes
-from . import utils
-from . import visualisation
-# from . import examples
-
--- src/build/lib/futuram/classes/__init__.py
+++
@@ -1,4 +0,0 @@
-from .processes import Process
-from .model import Model
-from .matter import Element, Compound, Material, Component, Product, Matter
-# from .parameters import Parameter, Scenario,
