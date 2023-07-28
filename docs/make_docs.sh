813c8ed999ce8dc0260df334879aac6aa6dcf651
SC-McD <stewart.charles.mcdowall@protonmail.ch>
2023-07-28 12:33:13 +0200

    update docs

--- docs/make_docs.sh
+++ docs/make_docs.sh
@@ -1,10 +1,10 @@
 #! bin/bash
-
+ghpush update-docs
 git checkout main
-cp -r docs/_build/* ~/tmp
+cp -r docs/_build ~/tmp
 git checkout gh-pages
-cp -r ~/tmp/* .
+cp -r ~/tmp/_build/* .
 git add .
 git commit -m "update docs"
 git push
-git checkout main
+#git checkout main
