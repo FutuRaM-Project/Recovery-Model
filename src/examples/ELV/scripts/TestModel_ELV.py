325dabe944efc19cf24a59e3884f013f3a7da5e7
deepj <das.deepj10@gmail.com>
2023-08-11 20:16:16 +0200

    TestModel_ELV_11August2023

--- src/examples/ELV/scripts/TestModel_ELV.py
+++ src/examples/ELV/scripts/TestModel_ELV.py
@@ -60,7 +60,7 @@ DIR_DATA = "../data/"
 DIR_DATA = os.path.join(dir_path, 'src', 'examples', 'ELV', 'data')
 
 # Take the composition, flow, process, TC xlsx and split it into csvs for each sheet
-from split_xlsx_to_csvs import xlsx_to_csvs
+
 DIR_COMPOSITIONS = f.utils.xlsx_to_csvs(f"{DIR_DATA}ELV_ICE_compositions_flows_processes_TCs.xlsx")
 
 
@@ -71,6 +71,8 @@ DIR_COMPOSITIONS = f.utils.xlsx_to_csvs(f"{DIR_DATA}ELV_ICE_compositions_flows_p
 
 
 # %% CREATE THE MODEL OBJECT
+
+
 
 # Create the empty model object
 model = f.classes.Model("TestModel_ELV")
