b9240878e011dc753b5ec6acb321fffd73712618
Harmjan de Vries <harmjandevries94@gmail.com>
2024-12-04 14:36:53 +0100

    update user guide

--- run_model.py
+++ run_model.py
@@ -10,7 +10,7 @@ pd.set_option("display.float_format", "{:.2f}".format)
 
 
 
-data_folder = "data_folder/test_2"
+data_folder = "data_folder/basic_test"
 layer_names = ['product','component','material','element']
 
 model = RecoveryModel(
